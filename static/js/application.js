be0141307ef756bc144c5ff27757f85aaa343fc4
Sai Deep Chand G <saideepchand.g@gmail.com>
2015-12-20 15:38:15 +0530

    Enhanced P2P Accounting page
    Ability to add/remove fields

--- static/js/application.js
+++ static/js/application.js
@@ -6,12 +6,48 @@ $("#form_filter").submit(function() {
         return false;
 
       }
-      else {    
-        
+      else {
         return true;
       }
     });
 
-$( document ).ready(function() {
-  ga('send', 'pageview', location.pathname);        
+$("#add-field").click(function() {
+        $('#add-field-form').toggle(100)
     });
+
+
+// Adding fields based on choices selected
+$ ('#add-field-form-btn').click(function(event) {
+
+	// check which journal source field is checked
+	if ($("#journal-source").is(':checked')) {
+		$('.jrnl-source-field').show();
+	} else {
+		$('.jrnl-source-field').hide();
+	}
+	// check which journal source field is checked
+	if ($("#defaults-from").is(':checked')) {
+		$('.defaults-from-field').show();
+	} else {
+		$('.defaults-from-field').hide();
+	}
+	if ($("#accting-class").is(':checked')) {
+		$('.accting-class-field').show();
+	} else {
+		$('.accting-class-field').hide();
+	}
+});
+
+// Toggle Sidebar and increase width of main accordingly
+$('.sidebar-toggle').click(function(event) {
+	/* Act on the event */
+	console.log ($(".sidebar").attr('display'));
+	$ ('.sidebar').toggle();
+	if($('.sidebar').css('display') == 'none') {
+
+		$(".main").attr("class", "col-sm-9 col-sm-offset-0 col-md-12 col-md-offset-0 main");
+	} else {
+		$(".main").attr("class", "col-sm-8 col-sm-offset-3 col-md-9 col-md-offset-3 main");
+	}
+
+});
